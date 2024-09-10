2b8afbf035eda7fc2b1e2fa167bb9f7befc2da9a
mino <1515069+minosss@users.noreply.github.com>
2024-09-10 19:35:29 +0800

    Rename removePathParams to excludePathParams

--- src/middleware/replaceUrlParams.ts
+++ src/middleware/replaceUrlParams.ts
@@ -6,17 +6,17 @@ function isObject(value: any): value is Record<string, any> {
 
 export interface ReplaceUrlParamsOptions {
   /**
-   * remove path params from input
+   * Exclude path params in input
    * @default true
    */
-  removePathParams?: boolean;
+  excludePathParams?: boolean;
 }
 
 /**
  * Replace url params with input. e.g. `/users/:id` to `/users/1`
  */
 export function replaceUrlParams(
-  options: ReplaceUrlParamsOptions = { removePathParams: true },
+  options: ReplaceUrlParamsOptions = { excludePathParams: true },
 ): MiddlewareFn {
   return async (ctx, next) => {
     // replace url params with input
@@ -32,7 +32,7 @@ export function replaceUrlParams(
           if (typeof value === "number" || typeof value === "string") {
             url = url.replace(param, value.toString().replace(/\s/g, ""));
             // remove path params
-            options.removePathParams && delete input[key];
+            options.excludePathParams && delete input[key];
           }
         } else {
           console.warn(`Bad params ${key} data.`);
