8e31b399bd8ce310fecab8619fd55f7c73f645ff
mino <1515069+minosss@users.noreply.github.com>
2024-11-15 14:44:50 +0800

    Add CI/CD workflow, update release process, and configure linting for API package

---
+++ packages/api/release.config.cjs
@@ -0,0 +1,86 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    'master',
+    'main',
+    { name: 'next', channel: 'next', prerelease: true },
+    { name: 'alpha', prerelease: 'alpha', channel: 'alpha' },
+    { name: 'beta', prerelease: 'beta', channel: 'beta' },
+  ],
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      preset: 'conventionalcommits',
+      releaseRules: [
+        { release: 'major', breaking: true },
+        { release: 'minor', type: 'feat' },
+        { release: 'patch', revert: true },
+        { release: 'patch', type: 'build' },
+        { release: 'patch', type: 'fix' },
+        { release: 'patch', type: 'perf' },
+        { release: 'patch', type: 'refactor' },
+        { type: 'ci', release: false },
+        { type: 'docs', release: false },
+        { type: 'chore', release: false },
+        { type: 'test', release: false },
+        { type: 'style', release: false },
+        { subject: '*skip release*', release: false },
+      ],
+      parserOpts: {
+        noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+      }
+    }],
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+      presetConfig: {
+        types: [
+          {
+            type: "revert",
+            section: "Reverts",
+            hidden: false,
+          },
+          {
+            type: "feat",
+            section: "Features",
+            hidden: false,
+          },
+          {
+            type: "fix",
+            section: "Bug Fixes",
+            hidden: false,
+          },
+          {
+            type: "perf",
+            section: "Performance improvements",
+            hidden: false,
+          },
+          {
+            type: "refactor",
+            section: "Refactors",
+            hidden: false,
+          },
+          {
+            type: "build",
+            section: "Build System",
+            hidden: false,
+          },
+          {
+            type: "docs",
+            section: "Documentation",
+            hidden: false,
+          },
+          { type: "chore", hidden: true },
+          { type: "test", hidden: true },
+          { type: "ci", hidden: true },
+          { type: "style", hidden: true },
+        ]
+      },
+      parserOpts: {
+        noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+      }
+    }],
+    ['@semantic-release/npm'],
+    ['@semantic-release/github']
+  ]
+}
